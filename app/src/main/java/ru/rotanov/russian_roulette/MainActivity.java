1717497bb294f185ef4a04e2695b85b450df2ba3
User <rotanowbter6545@gmail.com>
2024-02-17 17:44:03 +0500

    Доделал русскую рулетку

--- app/src/main/java/ru/rotanov/russian_roulette/MainActivity.java
+++ app/src/main/java/ru/rotanov/russian_roulette/MainActivity.java
@@ -7,8 +7,11 @@ import android.media.AudioAttributes;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.Random;
 import java.util.jar.Attributes;
 
 public class MainActivity extends Activity {
@@ -21,6 +24,13 @@ public class MainActivity extends Activity {
     private int soung_dumb;
 
     private ImageView blood_image;
+
+    private int on_shot;
+
+    private int random;
+
+    private TextView bullets;
+    private Button shot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +45,8 @@ public class MainActivity extends Activity {
                 .build();
         loadSounds();
         blood_image = findViewById(R.id.imageView2);
+        bullets = findViewById(R.id.count_bulets);
+        shot = findViewById(R.id.btn_shoot);
     }
 
 
@@ -45,17 +57,59 @@ public class MainActivity extends Activity {
     }
 
     public void onShot(View view) {
-        sounds.play(sound_Shot, 1.0f, 1.0f, 1, 0, 1);
-        blood_image.setVisibility(View.VISIBLE);
+        if(on_shot == random) {
+            sounds.play(sound_Shot, 1.0f, 1.0f, 1, 0, 1);
+            blood_image.setVisibility(View.VISIBLE);
+            shot.setEnabled(true);
+        } else{
+            onMissfire();
+            if (random < get_bullets()) {
+                random++;
+            } else {
+                random = 0;
+            }
+        }
     }
 
-    public void onMissfire(View view) {
+    public void onMissfire() {
         sounds.play(sound_missfire, 1.0f, 1.0f, 1, 0, 1);
         blood_image.setVisibility(View.INVISIBLE);
+
     }
 
     public void onDumb(View view) {
         sounds.play(soung_dumb, 1.0f, 1.0f, 1, 0, 1);
         blood_image.setVisibility(View.INVISIBLE);
+        on_shot = new Random().nextInt(get_bullets());
+        random = new Random().nextInt(get_bullets());
+        shot.setEnabled(true);
+    }
+
+    public void add_bullet(View view) {
+        int b = get_bullets();
+        if (b  == 11){
+            return;
+        }
+        b++;
+        bullets.setText(Integer.toString(b));
+    }
+
+
+
+
+    private int get_bullets(){
+        return Integer.parseInt(bullets.getText().toString());
+
+
+    }
+
+
+    public void remote_bullet(View view) {
+        int b = get_bullets();
+        if (b  == 3){
+            return;
+        }
+        b--;
+        bullets.setText(Integer.toString(b));
     }
 }
